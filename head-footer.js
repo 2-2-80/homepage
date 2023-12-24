033fbb9cff26c3fde34b17e004a07a7b9e7afc54
2-2-80 <87883457+2-2-80@users.noreply.github.com>
2023-12-24 21:58:56 +0900

    パス修正

--- head-footer.js
+++ head-footer.js
@@ -13,7 +13,7 @@ function header(){
 //フッター
 function footer(){
     $.ajax({
-        url: 'footer.html',
+        url: '/footer.html',
         cache: false,
         async: false,
         dataType: 'html',
