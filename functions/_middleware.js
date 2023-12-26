dd2ad6ae3a759ace742b0ba203e96374c2bdd044
2-2-80 <87883457+2-2-80@users.noreply.github.com>
2023-12-26 23:15:34 +0900

    _middleware.js を更新

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,69 +1,63 @@
-const Credentials = {
-  USERNAME: "user",
-  PASSWORD: "password",
+const credentials = {
+  '/R18': {
+    user: 'admin1',
+    pass: 'password1',
+  },
+  // 他のページも追加できます
 };
 
-
-const errorHandler = async ({ next }) => {
+async function errorHandling(context) {
   try {
-    return await next();
+    return await context.next();
   } catch (err) {
     return new Response(`${err.message}\n${err.stack}`, { status: 500 });
   }
-};
+}
 
+async function handleRequest({ next, request }) {
+  const url = new URL(request.url);
+  const pathMatch = Object.keys(credentials).find((path) => url.pathname.startsWith(path));
 
-const guardByBasicAuth = async ({ next, request, }) => {
-  // Check header
-  if (!request.headers.has("Authorization")) {
-    return new Response(
-      "You need to login.",
-      {
-        status: 401,
-        headers: {
-          // Prompts the user for credentials.
-          'WWW-Authenticate': 'Basic realm="Input username and password"',
-        },
-      });
-  };
-  // Decode header value
-  const [scheme, encoded] = request.headers.get("Authorization").split(' ');
-  if (!encoded || scheme !== 'Basic') {
-    return new Response(
-      "Malformed authorization header.",
-      {
-        status: 400,
+  if (pathMatch) {
+    if (request.headers.has("Authorization")) {
+      const Authorization = request.headers.get('Authorization');
+      const [scheme, encoded] = Authorization.split(' ');
+
+      if (!encoded || scheme !== 'Basic') {
+        return new Response(`The Authorization header must start with Basic`, {
+          status: 400,
+        });
       }
-    );
-  }
-  const buffer = Uint8Array.from(atob(encoded), character => character.charCodeAt(0));
-  const decoded = new TextDecoder().decode(buffer).normalize();
-  const index = decoded.indexOf(":");
-  if (index === -1 || /[\0-\x1F\x7F]/.test(decoded)) {
-    return new Response(
-      "Invalid authorization value.",
-      {
-        status: 400,
+
+      const buffer = Uint8Array.from(atob(encoded), (character) =>
+        character.charCodeAt(0)
+      );
+      const decoded = new TextDecoder().decode(buffer).normalize();
+      const index = decoded.indexOf(':');
+
+      if (index === -1 || /[\0-\x1F\x7F]/.test(decoded)) {
+        return new Response('Invalid authorization value.', { status: 400 });
       }
-    );
+
+      const user = decoded.substring(0, index);
+      const pass = decoded.substring(index + 1);
+
+      if (credentials[pathMatch].user !== user || credentials[pathMatch].pass !== pass) {
+        return new Response('Invalid credentials.', { status: 401 });
+      }
+
+      return await next();
+    }
+
+    return new Response('You need to login.', {
+      status: 401,
+      headers: {
+        'WWW-Authenticate': 'Basic realm="my scope", charset="UTF-8"',
+      },
+    });
   }
 
-  // Verify credentials
-  const creds = {
-      username: process.env.question || Credentials.USERNAME,
-      password: process.env.answer || Credentials.PASSWORD,
-  };
-  const username = decoded.substring(0, index);
-  const password = decoded.substring(index + 1);
-  if (username !== creds.username || password !== creds.password) {
-    return new Response(
-      "Invalid username or password.",
-      {
-        status: 401,
-      }
-    );
-  }
   return await next();
-};
+}
 
-export const onRequest = [errorHandler, guardByBasicAuth];
+export const onRequest = [errorHandling, handleRequest];
