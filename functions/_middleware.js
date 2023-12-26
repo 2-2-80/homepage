02355f3ecc5068d3f9f1be5fc9eb2138cbae7414
2-2-80 <87883457+2-2-80@users.noreply.github.com>
2023-12-27 00:16:21 +0900

    Update _middleware.js

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,7 +1,7 @@
 const credentials = async ({ request, next, env }) => {
   '/R18'; {
-    user: env.question,
-    pass: env.answer,
+    user: env.question ,
+    pass; env.answer ,
   },
   // 他のページも追加できます
 };
